3a46c330c37fcd31bc7ee770073921d0e74b104c
KVANTOVUIPROGRAMIST <48865602+KVANTOVUIPROGRAMIST@users.noreply.github.com>
2020-12-25 17:00:00 +0200

    Update medbay.py

--- medbay.py
+++ medbay.py
@@ -11,39 +11,39 @@ layout = [
     [s.Button("Green")],
     [s.Button("Blue")]
 ]
+def main():
+    window = s.Window("med", layout, margins=(300, 300))
 
-window = s.Window("med", layout, margins=(300, 300))
+    if b == 1:
+        color = (255, 0, 0)
+    if b == 2:
+        color = (0, 255, 0)
+    if b == 3:
+        color = (0, 0, 255)
 
-if b == 1:
-    color = (255, 0, 0)
-if b == 2:
-    color = (0, 255, 0)
-if b == 3:
-    color = (0, 0, 255)
+    img = Image.new('RGB', (100, 50), color)
+    imgDrawer = ImageDraw.Draw(img)
+    img.save("1.png")
 
-img = Image.new('RGB', (100, 50), color)
-imgDrawer = ImageDraw.Draw(img)
-img.save("1.png")
+    os.system("1.png")
 
-os.system("1.png")
-
-while True:
-    event, values = window.read()
-    if event == s.WIN_CLOSED:
-        break
-    elif event == "Red" and b == 1:
-        s.Popup("All clear u can go")
-        subprocess.Popen('main_ui.py 1', shell=True)
-        sys.exit(0)
-    elif event == "Green" and b == 2:
-        s.Popup("All clear u can go")
-        subprocess.Popen('main_ui.py 1', shell=True)
-        sys.exit(0)
-    elif event == "Blue" and b == 3:
-        s.Popup("All clear u can go")
-        subprocess.Popen('main_ui.py 1', shell=True)
-        sys.exit(0)
-    else:
-        s.Popup("test not passed!")
-        sys.exit(0)
-window.close()
+    while True:
+        event, values = window.read()
+        if event == s.WIN_CLOSED:
+            break
+        elif event == "Red" and b == 1:
+            s.Popup("All clear u can go")
+            subprocess.Popen('main_ui.py 1', shell=True)
+            sys.exit(0)
+        elif event == "Green" and b == 2:
+            s.Popup("All clear u can go")
+            subprocess.Popen('main_ui.py 1', shell=True)
+            sys.exit(0)
+        elif event == "Blue" and b == 3:
+            s.Popup("All clear u can go")
+            subprocess.Popen('main_ui.py 1', shell=True)
+            sys.exit(0)
+        else:
+            s.Popup("test not passed!")
+            sys.exit(0)
+    window.close()
